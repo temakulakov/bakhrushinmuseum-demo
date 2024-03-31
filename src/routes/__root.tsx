4522d08c90f2719ddf2febf335b55a20b61c3aa0
tema <artemkulakov250@gmail.com>
2024-03-31 22:59:24 +0300

    init

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,13 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { createRootRoute } from "@tanstack/react-router";
+// import { createRootRoute, Outlet } from "@tanstack/react-router";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <Outlet/>
-            <TanStackRouterDevtools/>
+            {/*<Outlet/>*/}
+            {/*<TanStackRouterDevtools/>*/}
         </>
     )
 
